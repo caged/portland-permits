d837fbdc42eac5ecf30750d5067ae43815675acd
Caged <justin@labratrevenge.com>
2017-10-29 13:28:10 -0700

    spike out notfound view

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom'
 import './index.css';
 import App from './App';
+import NotFound from './NotFound';
 import registerServiceWorker from './registerServiceWorker';
 
 const Root = () => {
